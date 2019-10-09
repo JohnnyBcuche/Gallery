dab4f58e7eb874f2253e4889b88588b23d9e73a6
Ivan <kisivan92@gmail.com>
2019-10-09 16:32:03 +0200

    new changes

--- src/components/Practice/Practice.js
+++ src/components/Practice/Practice.js
@@ -1,31 +1,64 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 
 import './Practice.css';
 
 class Practice extends Component {
+  token = null;
   state = {
-    items: []
-  }
+    query: '',
+    people: []
+  };
+
+  onChange = e => {
+    const { value } = e.target;
+    this.setState({
+      query: value
+    });
+
+    this.search(value);
+  };
+
+  search = query => {
+    const url = `https://swapi.co/api/people?search=${query}`;
+    const token = {};
+    this.token = token;
+
+    // axios.get(url)
+    // .then(res => {
+    //   const people = res.data;
+    //   if (this.token === token) {
+    //     this.setState({ people });
+    //   }
+    // })
+    fetch(url)
+    .then(results => results.json())
+    .then(data => {
+      if (this.token === token) {
+        this.setState({ people: data.results });
+      }
+    });
+  };
 
   componentDidMount() {
-    axios.get('https://jsonplaceholder.typicode.com/posts?userId=1')
-    .then(res => {
-      const items = res.data;
-      this.setState({ items });
-    })
+    this.search("");
   }
 
   render() {
     return (
-      <section>
-        { this.state.items.map(item =>
-          <div>
-            <h3>{item.title}</h3>
-            <p>{item.body}</p>
-          </div>
-        ) }
-      </section>
+      <form>
+        <input
+          type="text"
+          className="search-box"
+          placeholder="Search for..."
+          onChange={this.onChange}
+        />
+        {this.state.people.map(person => (
+          <ul key={person.name}>
+            <li>{person.name}</li>
+          </ul>
+        ))}
+      </form>
     );
   }
 }
